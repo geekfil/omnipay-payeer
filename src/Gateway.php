49f7b84a0dfb9a8acacf98404ad0e09e37873054
Igor Filin <geekfil@gmail.com>
2019-07-17 03:18:20 +0300

    fix class name

---
+++ src/Gateway.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Omnipay\Payeer;
+
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * Payeer Gateway
+ * @method \Omnipay\Common\Message\RequestInterface completeAuthorize(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface capture(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface purchase(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface completePurchase(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface refund(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface void(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface createCard(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface updateCard(array $options = array())
+ * @method \Omnipay\Common\Message\RequestInterface deleteCard(array $options = array())
+ */
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'Payeer';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'key' => '',
+            'testMode' => false,
+        );
+    }
+
+    public function getKey()
+    {
+        return $this->getParameter('key');
+    }
+
+    public function setKey($value)
+    {
+        return $this->setParameter('key', $value);
+    }
+
+    /**
+     * @return Message\AuthorizeRequest
+     */
+    public function authorize(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Payeer\Message\AuthorizeRequest', $parameters);
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface completeAuthorize(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface capture(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface purchase(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface completePurchase(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface refund(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface void(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface createCard(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface updateCard(array $options = array())
+        // TODO: Implement @method \Omnipay\Common\Message\RequestInterface deleteCard(array $options = array())
+    }
+}
