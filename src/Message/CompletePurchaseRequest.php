349c89546da9887b2dea2ac165d6af54d9eb65d0
Igor Filin <geekfil@gmail.com>
2019-08-02 21:48:56 +0300

    fix

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -15,49 +15,6 @@ class CompletePurchaseRequest extends AbstractRequest
     {
         return $this->setParameter('merchant_key', $value);
     }
-    public function setSuccessUrl($value)
-    {
-        return $this->setParameter('success_url', $value);
-    }
-
-    public function setFailUrl($value)
-    {
-        return $this->setParameter('fail_url', $value);
-    }
-
-    public function setStatusUrl($value)
-    {
-        return $this->setParameter('status_url', $value);
-    }
-
-    public function setParamsKey($value)
-    {
-        return $this->setParameter('params_key', $value);
-    }
-
-    public function setSubmerchant($value)
-    {
-        return $this->setParameter('submerchant',$value);
-    }
-
-    private function mParams()
-    {
-        $arParams = [
-            'success_url' => $this->getParameter('success_url'),
-            'fail_url' => $this->getParameter('fail_url'),
-            'status_url' => $this->getParameter('status_url'),
-            'submerchant' => $this->getParameter('submerchant')
-        ];
-
-        $key = md5($this->getParameter('params_key') . $this->getTransactionId());
-
-        return urlencode(
-            base64_encode(
-                @openssl_encrypt(
-                    json_encode($arParams),
-                    'AES-256-CBC', $key,
-                    OPENSSL_RAW_DATA)));
-    }
 
     public function getData()
     {
@@ -80,12 +37,14 @@ class CompletePurchaseRequest extends AbstractRequest
             $this->httpRequest->request->get('m_curr'),
             $this->httpRequest->request->get('m_desc'),
             $this->httpRequest->request->get('m_status'),
-            $this->getMerchantKey(),
         ];
 
         if ($this->httpRequest->request->has('m_params')){
             $arHash[] = $this->httpRequest->request->get('m_params');
         }
+
+        $arHash[] = $this->getMerchantKey();
+        
         $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
 
         if ($this->httpRequest->request->get('m_sign') != $sign_hash) {
